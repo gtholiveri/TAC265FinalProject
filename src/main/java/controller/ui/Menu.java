afb45b8400af70c2df18dad047332c0755396e05
Gabriel Oliveri <gabriel.oliveri2006@gmail.com>
2025-12-08 02:20:34 -0800

    Menu class: Stores a list of MenuOption objects each with an action that fires based on input Reuse code for display and stuff

--- src/main/java/controller/ui/Menu.java
+++ src/main/java/controller/ui/Menu.java
@@ -1,7 +1,36 @@
 package controller.ui;
 
-public abstract class Menu {
-    public static void main(String[] args) {
-        System.out.println("What is going on");
+import java.util.ArrayList;
+import java.util.List;
+
+import static util.Utils.validIndex;
+
+public abstract class Menu implements Displayable {
+    protected PageRankApp app;
+    protected List<MenuOption> options;
+
+    public Menu(PageRankApp app) {
+        this.app = app;
+        options = new ArrayList<>();
+        addOptions();
+    }
+
+    protected abstract void addOptions();
+
+
+    public void fire(int keyPressed) {
+        int i = keyPressed - 1;
+        if (validIndex(i, options.size())) {
+            options.get(i).fire();
+        }
+    }
+    
+    @Override
+    public String getDisplay() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < options.size(); i++) {
+            sb.append("(").append(i + 1).append(") ").append(options.get(i).getDisplay()).append("\n");
+        }
+        return sb.toString();
     }
 }
