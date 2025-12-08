e3259cb8b8bb992021560aa910b94317a9f82f91
Gabriel Oliveri <gabriel.oliveri2006@gmail.com>
2025-12-08 02:19:44 -0800

    Made the InputListener class, which uses this cool library that's able to natively capture keystroke events. Allows me to make the experience of using the app way better and cleaner because there's no need for scanner or pressing enter when doing simple things like selecting somethinig from a menu or turning a page.

---
+++ src/main/java/view/InputListener.java
@@ -0,0 +1,61 @@
+package view;
+
+import com.github.kwhat.jnativehook.GlobalScreen;
+import com.github.kwhat.jnativehook.NativeHookException;
+import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent;
+import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
+import controller.ui.PageRankApp;
+
+import java.text.NumberFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static com.github.kwhat.jnativehook.keyboard.NativeKeyEvent.getKeyText;
+
+
+
+public class InputListener implements NativeKeyListener {
+    private final PageRankApp app;
+
+    public InputListener(PageRankApp app) {
+        this.app = app;
+
+        try {
+            GlobalScreen.registerNativeHook(); // turns on the library's central listener
+        } catch (NativeHookException e) {
+            System.err.println("Critical error - there was a problem booting up the hook library.");
+            throw new RuntimeException(e);
+        }
+
+        GlobalScreen.addNativeKeyListener(this); // registers this object as a listener for key events
+        disableJNativeHookLogging();
+    }
+
+    @Override
+    public void nativeKeyPressed(NativeKeyEvent event) {
+        Printer.println("KEY PRESSED (" + getKeyText(event.getKeyCode()) + ")");
+        app.handleKey(getKeyText(event.getKeyCode()));
+    }
+
+
+    /**
+     * JNativeHook outputs a lot of logs, so this subroutine disables them.
+     */
+    private void disableJNativeHookLogging() {
+        Logger logger = Logger.getLogger(GlobalScreen.class.getPackage().getName());
+        logger.setLevel(Level.OFF);
+        logger.setUseParentHandlers(false);
+    }
+
+    public void close() {
+        try {
+            GlobalScreen.removeNativeKeyListener(this);
+            GlobalScreen.unregisterNativeHook();
+        } catch (NativeHookException e) {
+            // Stuff went real wrong, we just eat this one
+            System.err.println("Critical error - there was a problem shutting down the hook library.");
+            throw new RuntimeException(e);
+        }
+    }
+
+}
