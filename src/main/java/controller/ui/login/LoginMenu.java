341ef5c8bc2ed3f0feb0eb7d588f33b6649a7e59
Gabriel Oliveri <gabriel.oliveri2006@gmail.com>
2025-12-08 02:33:25 -0800

    Made the LoginMenu class, which is going to be the first of many Inheritance makes it pretty seamlessly clean to define a whole new menu, which is really nice The reason I'm defining it is way is to make it easy to make a lot of them and make them link to each other / have a sort of tree structure

---
+++ src/main/java/controller/ui/login/LoginMenu.java
@@ -0,0 +1,19 @@
+package controller.ui.login;
+
+import controller.ui.Menu;
+import controller.ui.PageRankApp;
+
+public class LoginMenu extends Menu {
+
+
+    public LoginMenu(PageRankApp app) {
+        super(app);
+    }
+
+    @Override
+    protected void addOptions() {
+        options.add(new LoginOption(app));
+        options.add(new CreateAccountOption(app));
+        options.add(new ExitOption(app));
+    }
+}
