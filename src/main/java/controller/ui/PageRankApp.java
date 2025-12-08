9b7bc4d19ce53bae95eed55925aa9567d3533aec
Gabriel Oliveri <gabriel.oliveri2006@gmail.com>
2025-12-08 15:22:12 -0800

    Big refactor to move away from latch approach which was having terrible issues to much nicer BlockingQueue producer-consumer pattern. Way better separation of concerns and no more thread shenanigans.

--- src/main/java/controller/ui/PageRankApp.java
+++ src/main/java/controller/ui/PageRankApp.java
@@ -4,7 +4,8 @@ import controller.ui.login.LoginMenu;
 import view.InputListener;
 import view.Printer;
 
-import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 import static controller.ui.AppState.*;
 
@@ -13,8 +14,7 @@ public class PageRankApp {
 
     private AppState state = AppState.WAITING_FOR_INPUT;
 
-    private CountDownLatch inputLatch;
-    private CountDownLatch stallLatch;
+    private BlockingQueue<String> inputQueue;
 
     public static void main(String[] args) {
         PageRankApp app = new PageRankApp();
@@ -26,8 +26,7 @@ public class PageRankApp {
         currMenu = new LoginMenu(this);
         new InputListener(this);
 
-        inputLatch = new CountDownLatch(0);
-        stallLatch = new CountDownLatch(0);
+        inputQueue = new ArrayBlockingQueue<>(1);
 
     }
 
@@ -36,95 +35,51 @@ public class PageRankApp {
      * - Render the current menu<br>
      * - wait for user input<br>
      */
-
-    /*
-     * GAHHH
-     * this is pissing me off so bad
-     * What I have discovered: Unintentionally making multithreaded applications makes things HARD
-     * I think I know exactly what the issue is:
-     * - upon receiving valid input while state = WAITING_FOR_INPUT, the InputListener fires, so the call stack goes:
-     *         - nativeKeyPressed() -> handleKey() -> currMenu.fire(int) -> option.fire() -> stall()
-     * - that somehow forces stall() to execute ON THE LISTENER'S THREAD
-     * - which blocks the listener itself from receiving any further input
-     *
-     * Solution: I think I need to use a BlockingQueue of some sort to have like a producer consumer pattern to decouple the InputListener from
-     * actually calling methods in PageRankApp, which I really shouldn't have been doing in the first place if I had been properly enforcing MVC separation
-     *
-     * I think I shied away from this previously because I was like "but what if multiple inputs stack in the queue?"
-     * But that really won't be an issue / won't happen, it's fast enough that it's a non issue and real apps
-     * let inputs stack up all the time, and it actually enhances the experience a lot of the time
-     *
-     * But that took me way too long to figure out so I'm going to just go to bed and fix later.
-     * Basic UI concept is very solid I think, simultaneously massively enhances the usual terminal UI functionality
-     * and involves multiple interlacing hierarchies
-
-     */
     public void execute() {
-        // TODO: implement the main execution loop of the whole app
         Printer.println("Welcome to PageRank, the competitive reading app!");
         stall();
         while (state != EXIT) {
-            Printer.print(currMenu.getDisplay());
+            Printer.clearPrintln(currMenu.getDisplay());
             waitForInput();
         }
     }
 
-    public void handleKey(String keyText) {
-        switch (state) {
-            case WAITING_FOR_INPUT -> {
-                inputLatch.countDown();
-//                Printer.println("LATCH WAS COUNTED DOWN, IT'S NOW AT: " + inputLatch.getCount());
-
-
-                try {
-//                    Printer.println("ABOUT TO FIRE MENU WITH KEY " + keyText);
-                    currMenu.fire(Integer.parseInt(keyText));
-//                    Printer.println("JUST FIRED MENU WITH KEY " + keyText);
-                } catch (NumberFormatException e) {
-                    // do nothing, since non-numeric input isn't valid input
-//                    Printer.println("WE TRIED TO PARSE IT BUT IT WAS BAD");
-                }
-            }
-            case STALLING -> {
-                state = WAITING_FOR_INPUT;
-                stallLatch.countDown();
-            }
-            case EXIT -> {
-                // do nothing, the main loop will naturally terminate
-                Printer.println("What the fuck are we doing in here");
-            }
-        }
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    public void queueInput(String input) {
+        inputQueue.offer(input);
     }
 
     private void waitForInput() {
         state = WAITING_FOR_INPUT;
-        Printer.println("WAITING FOR INPUT");
-        inputLatch = new CountDownLatch(1);
-
 
         try {
-            inputLatch.await();
+            // block until we get some input that is valid
+            String input = "";
+            while (!currMenu.validInput(input)) {
+                input = inputQueue.take();
+            }
+
+            // once we get valid input, fire the menu off of that
+            // this might be fragile or there might be a cleaner way to do this but I think it's fairly readable
+            currMenu.fire(Integer.parseInt(input));
+            
         } catch (InterruptedException e) {
             System.err.println("Interrupted while waiting for input.");
             throw new RuntimeException(e);
         }
-        Printer.println("WENT THROUGH INPUT LATCH");
     }
 
     public void stall() {
-//        Printer.println("Press anything to continue.");
-        Printer.println("STALLING FOR ANY KEY PRESS");
+        Printer.println("Press anything to continue.");
         state = STALLING;
-        stallLatch = new CountDownLatch(1);
 
         try {
-//            Printer.println("WAITING FOR STALLED CONTINUE CONFIRMATION");
-
-            stallLatch.await();
+            // block until we get some input, doesn't matter what it is
+            inputQueue.take();
         } catch (InterruptedException e) {
-            System.err.println("Interrupted while waiting for input.");
+            System.err.println("Interrupted while stalling for confirmation key press.");
             throw new RuntimeException(e);
         }
-        Printer.println("WENT THROUGH STALL LATCH");
+
     }
 }
