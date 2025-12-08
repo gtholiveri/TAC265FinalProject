3460915684429e3655f292be3c0a343c157f15a8
Gabriel Oliveri <gabriel.oliveri2006@gmail.com>
2025-12-08 02:23:41 -0800

    Interface for things that can be displayed This will probably have an actual interface-y use case in the future when I hope to have collections of Displayables in more complex classes (because there's no point in slapping interfaces on things that you're never going to leverage the common property for)

---
+++ src/main/java/controller/ui/Displayable.java
@@ -0,0 +1,11 @@
+package controller.ui;
+
+/**
+ * Interface for objects that can be displayed to the user in some way.
+ * It might not be strictly necessary, but it could be useful in the ereader context since that well
+ * have a very heterogeneous set of elements, has pagination nonsense etc so idk<br>
+ * For now it's just a fancy label
+ */
+public interface Displayable {
+    public String getDisplay();
+}
