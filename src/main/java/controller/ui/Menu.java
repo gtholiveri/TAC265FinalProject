49b08da5ab693595a8350991d4ed4612121a2dc0
Gabriel Oliveri <gabriel.oliveri2006@gmail.com>
2025-12-08 15:21:10 -0800

    Added validInput(String) method Useful for knowing in PageRankApp when to actually stop polling for valid input in the queue and call fire()

--- src/main/java/controller/ui/Menu.java
+++ src/main/java/controller/ui/Menu.java
@@ -33,4 +33,20 @@ public abstract class Menu implements Displayable {
         }
         return sb.toString();
     }
+
+    /**
+     * @param input A String containing a key pressed by the user
+     * @return true if it's an int within the valid range of menu options, false otherwise
+     */
+    public boolean validInput(String input) {
+        try {
+            int sel = Integer.parseInt(input);
+            int i= sel - 1;
+            return validIndex(i, options.size());
+
+        } catch (NumberFormatException e) {
+            // not a number
+            return false;
+        }
+    }
 }
