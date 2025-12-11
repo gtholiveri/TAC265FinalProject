427f48407cff70368006b38e91100cf6955773c0
Gabriel Oliveri <gabriel.oliveri2006@gmail.com>
2025-12-11 01:05:20 -0800

    Added hash library

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,11 +10,8 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
     implementation("com.github.kwhat:jnativehook:2.2.2")
+    implementation("org.mindrot:jbcrypt:0.4")
 }
 
 application {
