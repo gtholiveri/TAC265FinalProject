a45f77c14786ae88efbf26f7156fcb575d05b881
Gabriel Oliveri <gabriel.oliveri2006@gmail.com>
2025-12-11 01:08:00 -0800

    Pretty printing enum

---
+++ src/main/java/view/TextFormat.java
@@ -0,0 +1,21 @@
+package view;
+
+/**
+ * Enum representing text formatting options using ANSI escape codes.
+ * Can be combined using EnumSet for multiple simultaneous formats.
+ */
+public enum TextFormat {
+    BOLD("\033[1m"),
+    ITALIC("\033[3m"),
+    UNDERLINE("\033[4m");
+
+    private final String code;
+
+    TextFormat(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+}
